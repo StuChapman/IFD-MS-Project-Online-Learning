e16450b2730887d249f34514761fdb1fd5842fd8
Stuart Chapman <chapman.stuart@sky.com>
2020-07-09 15:20:18 +0000

    Build: help email functional. User_ID and sendername bugs fixed

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,11 +1,22 @@
-//Credit: Code Institute//
-function sendEmail(contactForm) {
-    console.log("launched");
-    emailjs.send("continuous_engagement", "template_6DMLrcJu", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailinput.value,
-        "question": contactForm.question.value
-    })
+
+function sendEmail() {
+    console.log(this.sendername.value);
+    console.log(this.emailinput.value);
+    console.log(this.question.value);
+
+    emailjs.init("user_37585cYmkMNZRiOobd27i")
+
+    var template_params = {
+    "from_name": this.sendername.value,
+    "from_email": this.emailinput.value,
+    "question": this.question.value
+    }
+
+    var service_id = "continuous_engagement";
+    var template_id = "template_6DMLrcJu";
+
+    emailjs.send(service_id, template_id, template_params)
+    //Credit: Code Institute//
     .then(
         function(response) {
             console.log("success", response);
