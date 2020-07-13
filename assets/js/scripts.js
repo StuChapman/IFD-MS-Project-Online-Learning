15d955efab70eb356aa2d18ef9fd0f298bfca4fe
Stuart Chapman <chapman.stuart@sky.com>
2020-07-13 11:18:11 +0000

    Build: added nextReveal function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,8 @@
+//Set global variables //
+
+var nextReveal = 0;
+var nextRevealValue = 0;
+var nextRevealWaste = 0;
 
 //Function: use emailjs account to email a question from the help? button on the header //
 function sendEmail() {
@@ -6,7 +11,7 @@ function sendEmail() {
     console.log(this.question.value);
     console.log(document.title);
 
-    emailjs.init("user_37585cYmkMNZRiOobd27i")
+    emailjs.init("user_37585cYmkMNZRiOobd27i");
 
     var thispage = document.title;
     var template_params = {
@@ -33,6 +38,8 @@ function sendEmail() {
 //Function: reveal the definition for Value by clicking to word value on definition.html //
 function revealValue() { //Credit: https://www.tutorialrepublic.com/codelab.php?topic=faq&file=jquery-slide-left-and-right-effect
     console.log('Value');
+    nextRevealValue++;
+    revealNext();
     $("#arrowmask").animate({
                 width: '30vw'
             });
@@ -43,6 +50,8 @@ function revealValue() { //Credit: https://www.tutorialrepublic.com/codelab.php?
 
 function revealWaste() { //Credit: https://www.tutorialrepublic.com/codelab.php?topic=faq&file=jquery-slide-left-and-right-effect
     console.log('Waste');
+    nextRevealWaste++
+    revealNext();
     $("#arrowvalue").animate({
                 width: 0
             });
@@ -51,7 +60,18 @@ function revealWaste() { //Credit: https://www.tutorialrepublic.com/codelab.php?
             });
 }
 
-//Function: using the play button to play the Mark Onetto video on looking.html /
+//Function: revealing the nextLink after content has been interacted with //
+function revealNext() {
+    nextReveal = nextRevealValue * nextRevealWaste;
+    console.log(nextReveal);
+    if (nextReveal > 0) {
+         var el = document.getElementById("nextLink");
+            el.classList.remove("hidden");
+            el.classList.add("unhidden");
+    }
+}
+
+//Function: using the play button to play the Mark Onetto video on looking.html//
 function playVid() { //Credit: https://www.w3schools.com/tags/av_met_play.asp
 
     var buttonText = $("#playbutton").text();
